5a26f13f17e34d837d1630b41ccd3d7b360f9b70
Lagniaux Jean <34351725+jeanlagniaux@users.noreply.github.com>
2021-03-11 19:14:55 +0100

    Try rest api on card and user

--- src/main/java/fr/istic/taa/jaxrs/rest/CardResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/CardResource.java
@@ -6,32 +6,40 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import fr.istic.taa.jaxrs.dao.generic.CardDao;
+import fr.istic.taa.jaxrs.domain.Card;
 import fr.istic.taa.jaxrs.domain.Pet;
 import fr.istic.taa.jaxrs.domain.User;
 import io.swagger.v3.oas.annotations.Parameter;
 
 @Path("/card")
-@Produces({ "application/json"})
+@Produces({ "application/json" })
 public class CardResource {
 
 	CardDao daoC = new CardDao();
+
+	@GET
+	@Path("/{id}/users")
+	public List<User> getAffectedUser(@PathParam("id") int id) {
+		return daoC.getCardUsersById((long) id);
+	}
 	
 	@GET
 	@Path("/{id}")
-	public List<User> getAffectedUser() {
-		return daoC.getAffectedUser();
+	public Card getCardById(@PathParam("id") int id) {
+		return daoC.getCardById((long) id);
 	}
 
 	@POST
 	@Consumes("application/json")
-	public Response addCard(@Parameter(description = "Pet object that needs to be added to the store", required = true) Pet pet) {
+	public Response addCard(
+			@Parameter(description = "Pet object that needs to be added to the store", required = true) Pet pet) {
 		// add pet
 		return Response.ok().entity("SUCCESS").build();
 	}
-	
-	
+
 }
--- src/main/java/fr/istic/taa/jaxrs/rest/UserResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/UserResource.java
@@ -9,28 +9,30 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import fr.istic.taa.jaxrs.dao.generic.UserDao;
+import fr.istic.taa.jaxrs.domain.Card;
 import fr.istic.taa.jaxrs.domain.User;
 import io.swagger.v3.oas.annotations.Parameter;
 
 @Path("/user")
-@Produces({"application/json"})
-public class UserResource{
+@Produces({ "application/json" })
+public class UserResource {
 
 	UserDao daoU = new UserDao();
-	
+
 	@GET
 	@Path("/{mail}")
 	public User getUserByMail(@PathParam("mail") String email) {
 		return daoU.getUserByMail(email);
 	}
-	
-	
+
 	@POST
 	@Consumes("application/json")
 	@Path("/addUser")
-	//si on veut rajouter une nouvelle classe en param il faut faire un nouveau @param
+	// si on veut rajouter une nouvelle classe en param il faut faire un nouveau
+	// @param
 	public Response addUser(@Parameter(description = "add user", required = true) User user) {
-		// java va comprendre grace au JSON envoyée dans la requette de quelle objet on parle
+		// java va comprendre grace au JSON envoyée dans la requette de quelle objet on
+		// parle
 		daoU.save(user);
 		return Response.ok().entity("SUCCESS").build();
 	}
