6dc7dd3136915ffc7427ae7fb1c7ebf7137f4d55
Lagniaux Jean <34351725+jeanlagniaux@users.noreply.github.com>
2021-04-29 17:54:02 +0200

    Update CardResource.java

--- src/main/java/fr/istic/taa/jaxrs/rest/CardResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/CardResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import fr.istic.taa.jaxrs.dao.generic.CardDao;
@@ -41,7 +42,7 @@ public class CardResource {
 	}
 
 	@POST
-	@Consumes("application/json")
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/addCard")
 	public Response addCard(@Parameter(description = "add user", required = true) Card card) {
 		daoC.save(card);
